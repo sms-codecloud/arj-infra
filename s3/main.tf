e3761b20cd0270721f35c86c06a4df6efb339385
shankarbk <sbk.qubits@gmail.com>
2025-10-02 17:02:37 +0530

    aws_s3_bucket_public_access_block

--- s3/main.tf
+++ s3/main.tf
@@ -1,9 +1,18 @@
 # Create S3 bucket
 resource "aws_s3_bucket" "this" {
   bucket = "arj-bootcamp"
+  force_destroy = true
 
   tags = {
     Name        = "shankar-arj"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "ui" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
